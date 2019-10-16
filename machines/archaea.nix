ffb9ecb2fe6e3c69ef335f88f6b227c671e88c0f
System administrator <breakds@gmail.com>
2019-10-16 19:34:55 +0000

    Update system.stateVersion

--- machines/archaea.nix
+++ machines/archaea.nix
@@ -10,4 +10,10 @@
   networking.hostName = "archaea";
   # Generated via `head -c 8 /etc/machine-id`
   networking.hostId = "867ce369";
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
 }
--- machines/base.nix
+++ machines/base.nix
@@ -92,10 +92,4 @@
   # +------------------------------------------------------------+
   # | Version Settings                                           |
   # +------------------------------------------------------------+
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.03"; # Did you read the comment?
 }
