0bbca0d6cb73cef0bc311cc9947669e223dc7149
Break Yang <breakds@gmail.com>
2019-11-17 05:56:34 +0000

    Add rival

---
+++ machines/rival.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./base.nix
+    ../nix-home
+    ../modules/user.nix
+    ../modules/desktop
+  ];
+
+  # Machine-specific networking configuration.
+  networking.hostName = "rival";
+  # Generated via `head -c 8 /etc/machine-id`
+  networking.hostId = "efa94cac";
+
+  # +----------+
+  # | Desktop  |
+  # +----------+
+
+  bds.desktop = {
+    enable = true;
+    xserver.displayManager = "gdm";
+    xserver.i3_show_battery = true;
+  };
+}
