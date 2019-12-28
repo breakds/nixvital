3f07a6bd6271999821fb8846c39ec25c2682da90
Break Yang <breakds@gmail.com>
2019-12-27 22:50:36 -0800

    Add Shan's first machine.

---
+++ machines/cassandra/desktop.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../base.nix
+    ../../nix-home
+    ../../modules/user.nix
+    ../../modules/desktop
+    ../../modules/web
+  ];
+
+  vital.machineType = "desktop";
+
+  # +----------+
+  # | Desktop  |
+  # +----------+
+
+  vital.desktop = {
+    enable = true;
+    xserver.displayManager = "gdm";
+    xserver.dpi = 100;
+    nvidia = {
+      enable = true;
+    };
+  };
+
+  # +------------+
+  # | WakeOnLan  |
+  # +------------+
+
+  services.wakeonlan.interfaces = [{
+    interface = "eno1";
+  }];
+}
