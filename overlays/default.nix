bcff367f6a4b6f99e632c2ee4ed991a337819d3c
Break Yang <breakds@gmail.com>
2020-01-26 21:49:44 -0800

    Backport hugo 0.62.

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,6 +4,11 @@ let old-jetbrains-nixpkgs = import (builtins.fetchTarball {
       url = https://github.com/NixOS/nixpkgs/tarball/83c627b8b0e40d14aed0b02ecb3c303444152a8b;
     }) { config.allowUnfree = true; };
 
+    backportPkgs = import (builtins.fetchTarball {
+      # 2020 Jan 26
+      url = https://github.com/NixOS/nixpkgs/tarball/05e661f665047984189b96c724f5a5a1745ec7cb;
+    }) { config.allowUnfree = true; };
+
 in {
   # LLVM series
   inherit (super.llvmPackages_8) clang libclang llvm;
@@ -39,4 +44,7 @@ in {
 
   # Override the jetbrains with an older version, where clion supports bazel.
   old-jetbrains = old-jetbrains-nixpkgs.jetbrains;
+
+  # Backport the newer version of hugo
+  hugo = backportPkgs.hugo;
 }
