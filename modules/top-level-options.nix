1424309f2a703fa97e634b30ba501ce4c7e473e5
Break Yang <yiqing.yang@weride.ai>
2020-02-11 23:06:35 -0800

    Make the lid behavior for laptop more correct.

--- modules/top-level-options.nix
+++ modules/top-level-options.nix
@@ -36,10 +36,19 @@ in {
 
     # Handle lids for laptops.
     services.logind = lib.mkIf (cfg.machineType == "laptop") {
-      extraConfig = "HandleLidSwitch=ignore";
+      # The following settings configures the following behavior for laptops
+      # When the lid close event is detected,
+      #   1. If the external power is on, do nothing
+      #   2. If the laptop is docked (external dock or monitor or hub), do nothing
+      #   3. Otherwise, it should go to suspend and then hibernate. However this action
+      #      will be held off for 20 seconds to wait for the users to dock or plug
+      #      external power.
       lidSwitch = "suspend-then-hibernate";
       lidSwitchDocked = "ignore";
       lidSwitchExternalPower = "ignore";
+      extraConfig = ''
+        HoldoffTimeoutSec=20
+      '';
     };
   };
 }
