46e89b138996c918db47abbaa50dc469b75b0ca4
Break Yang <breakds@gmail.com>
2020-02-16 22:41:49 -0800

    Enable terraria.

--- modules/services/terraria.nix
+++ modules/services/terraria.nix
@@ -2,6 +2,7 @@
 
 {
   services.terraria = {
+    enable = true;
     port = (import ../../data/resources.nix).ports.terraria;
     autoCreatedWorldSize = "medium";
     messageOfTheDay = "Welcome to the Farm.";
