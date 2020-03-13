a7a69ef8cb8cdf8408d784969062abf892f1d4c6
Break Yang <yiqing.yang@weride.ai>
2020-03-13 13:54:56 -0700

    IDE Support.

--- modules/dev/vscode.nix
+++ modules/dev/vscode.nix
@@ -40,6 +40,20 @@ let
       version = "2.19.0";
       sha256 = "0lgf1a7a8ikvkas4g69w1n5i3s5pabs6d394na2nz0iib490kkw1";
     }
+    # Syntax highlighting for BUILD files.
+    {
+      name = "bazel-code";
+      publisher = "DevonDCarew";
+      version = "0.1.9";
+      sha256 = "0lsb4vlqwqqlm0yzljhl8sl151j41lxlpj9wh82m90v59qibpkkf";
+    }
+    # Bazel Plugin for Projects
+    {
+      name = "vscode-bazel";
+      publisher = "BazelBuild";
+      version = "0.3.0";
+      sha256 = "0rlja1hn2n6fyq673qskz2a69rz8b0i5g5flyxm5sfi8bcz8ms05";
+    }
   ];
   
   customized-vscode = pkgs.vscode-with-extensions.override {
