b87f1e61ede0ce93d6dce2455d96a0b0231c675e
Break Yang <breakds@gmail.com>
2020-03-29 11:06:36 -0700

    Add nixvital-web-installer to graphical livecd, and remove xfce/mate version.

--- livecd/nixos_graphical_iso.nix
+++ livecd/nixos_graphical_iso.nix
@@ -8,8 +8,13 @@
     # Provide an initial copy of the NixOS channel so that the user
     # doesn't need to run "nix-channel --update" first.
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    # Start the installer service in the background.
+    ../modules/services/nixvital-web-installer.nix
   ];
 
+  # Disable wayland.
+  services.xserver.displayManager.gdm.wayland = false;
+  
   environment.systemPackages = with pkgs; [
     vim emacs firefox git
   ];
