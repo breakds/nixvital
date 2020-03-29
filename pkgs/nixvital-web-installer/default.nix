49f94efbc4f255716fc70d5bb0313ec7b4595d9b
Break Yang <breakds@gmail.com>
2020-03-29 00:52:11 -0700

    Upgrade nixvital-web-installer to 0.4.0

--- pkgs/nixvital-web-installer/default.nix
+++ pkgs/nixvital-web-installer/default.nix
@@ -2,15 +2,15 @@
 
 pkgs.python3Packages.buildPythonApplication rec {
   pname = "nixvital-web-installer";
-  version = "0.3.1";
+  version = "0.4.0";
 
   src = pkgs.fetchgit {
     url = "https://github.com/breakds/nixvital-web-installer.git";
-    rev = "65bc629fc0740a21197b8b6c1032b6187ec0afa1";
-    sha256 = "1n8wcna31hf9zx22bpqwr3bbylflil2293cb5ccrhmq108brx08l";
+    rev = "0.4.0";
+    sha256 = "0bgyam90i798f3fn9mr16h8wzpbvcz35i82mi2h6j1x6zxf40r1s";
   };
 
   propagatedBuildInputs = with pkgs.python3Packages; [
-    click flask psutil GitPython
+    click flask psutil GitPython pyyaml
   ];
 }
