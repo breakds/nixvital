f60fa1602b0039109ba08c1e30df0c36e0ea6c54
Break Yang <breakds@gmail.com>
2020-04-10 17:41:14 -0700

    Update the steam configuration.

--- modules/steam.nix
+++ modules/steam.nix
@@ -4,7 +4,7 @@
 
 let my_steam = pkgs.steam.override {
       # Use nixos libraries instead of steam-provided
-      # nativeOnly = true;
+      nativeOnly = true;
       withJava = true;
       extraPkgs = p: [
         pkgs.openldap
@@ -12,6 +12,19 @@ let my_steam = pkgs.steam.override {
       ];
   };
 in {
+  nixpkgs.config.allowBroken = true;
+
+  # https://github.com/NixOS/nixpkgs/issues/45492#issuecomment-418903252
+  # Set limits for esync.
+  systemd.extraConfig = "DefaultLimitNOFILE=1048576";
+
+  security.pam.loginLimits = [{
+    domain = "*";
+    type = "hard";
+    item = "nofile";
+    value = "1048576";
+  }];
+  
   environment.systemPackages = with pkgs; [
     wine
     my_steam
