d3a57e92c86595c2ff889d84642020c2f31ed345
Break Yang <breakds@gmail.com>
2020-05-24 11:08:21 -0700

    Update blog.

--- pkgs/www-breakds-org/default.nix
+++ pkgs/www-breakds-org/default.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation {
   
   src = pkgs.fetchgit {
     url = "https://github.com/breakds/www.breakds.org.git";
-    rev = "878814d0ca856a6b912f170b7b66a06ea6c7bece";
-    sha256 = "022qwganwsxj1w070hb2l2p5vb0g0vvdav5yvf2pcswhjhr5vad2";
+    rev = "7fb3f4a476edee1f9efbbe30226d4596fa570d98";
+    sha256 = "0ckqr041lzfylpvgc579zfryrpxv17wdgnlv1xlahd3j4pa027k7";
     fetchSubmodules = true;
   };
 
