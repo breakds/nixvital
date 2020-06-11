882b142331108b42e9f01b92738761ecd6b0f831
Break Yang <breakds@gmail.com>
2020-06-10 22:15:44 -0700

    Backported hugo is no longer needed.

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,11 +4,6 @@ let old-jetbrains-nixpkgs = import (builtins.fetchTarball {
       url = https://github.com/NixOS/nixpkgs/tarball/83c627b8b0e40d14aed0b02ecb3c303444152a8b;
     }) { config.allowUnfree = true; };
 
-    backportPkgs = import (builtins.fetchTarball {
-      # 2020 Jan 26
-      url = https://github.com/NixOS/nixpkgs/tarball/05e661f665047984189b96c724f5a5a1745ec7cb;
-    }) { config.allowUnfree = true; };
-
     unstablePkgs = import (builtins.fetchTarball {
       # 2020 May 17
       url = https://github.com/NixOS/nixpkgs/tarball/548872be20dcb78cdb1a554dcef51caf1d6055ca;
@@ -59,9 +54,6 @@ in {
 
   # Override the jetbrains with an older version, where clion supports bazel.
   old-jetbrains = old-jetbrains-nixpkgs.jetbrains;
-
-  # Backport the newer version of hugo
-  hugo = backportPkgs.hugo;
 
   # www.breakds.org, the personal website
   www-breakds-org = self.callPackage ../pkgs/www-breakds-org {};
