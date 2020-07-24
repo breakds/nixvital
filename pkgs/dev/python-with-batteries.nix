64d779eeb00f1189a58f720de0f935aed8e19768
Break Yang <breakds@gmail.com>
2020-07-24 09:38:48 -0700

    Add imageio as default package for python dev.

--- pkgs/dev/python-with-batteries.nix
+++ pkgs/dev/python-with-batteries.nix
@@ -7,8 +7,24 @@ python3.withPackages (
     let the-torchvision = torchvision.override {
           pytorch = pytorchWithCuda;
         };
-    in [ numpy pandas matplotlib jupyterlab plotly dash tqdm ]
-       ++ (lib.optionals enableMachineLearning [ lightgbm pytorchWithCuda the-torchvision opencv4 ])
+
+        computation = [
+          numpy pandas
+        ];
+
+        ide = [
+          jupyterhub
+        ];
+
+        viz = [
+          matplotlib plotly dash tqdm
+        ];
+
+        image = [
+          opencv4 imageio
+        ];
+    in computation ++ ide ++ viz ++ image
+       ++ (lib.optionals enableMachineLearning [ lightgbm pytorchWithCuda the-torchvision ])
         # FIXME: Currently jupyterhub does not yet work with jupyter
         # lab. Will make this working later.
        ++ (lib.optionals enableJupyterhub [ jupyterhub ]))
