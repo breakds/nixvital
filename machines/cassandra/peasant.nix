da7eee0e1ace2761bddc79793b154edc1b9a0f9d
Break Yang <breakds@gmail.com>
2020-11-03 18:04:36 -0800

    Add peasant as a new machine.

---
+++ machines/cassandra/peasant.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../base
+    ../../nix-home/cassandra
+    ../../modules/users
+    ../../modules/desktop
+    ../../modules/dev/vscode.nix
+    ../../modules/dev/python-environment.nix
+    ../../modules/IoT/apple-devices.nix
+    ../../modules/steam.nix
+  ];
+
+  vital.machineType = "laptop";
+
+  vital.dev.python = {
+    batteries.machineLearning = false;
+  };
+
+  # +----------+
+  # | Desktop  |
+  # +----------+
+
+  vital.desktop = {
+    enable = true;
+    xserver.displayManager = "gdm";
+  };
+
+  # +----------+
+  # | Extras   |
+  # +----------+
+
+  environment.systemPackages = with pkgs; [
+    gimp peek gnupg pass libreoffice
+    nodejs-12_x
+    skypeforlinux
+  ];
+}
