f0e4a5637de6f0d4917ed382982b655f56cb256e
Break Yang <breakds@gmail.com>
2020-11-07 08:16:03 -0800

    Nvidia prime adapt to nixos 20.09

--- modules/desktop/nvidia.nix
+++ modules/desktop/nvidia.nix
@@ -11,15 +11,15 @@ in lib.mkIf cfg.enable {
     # Nvidia PRIME The card Nvidia 940MX is non-MXM card. Needs special treatment.
     # muxless/non-MXM Optimus cards have no display outputs and show as 3D
     # Controller in lspci output, seen in most modern consumer laptops
-    nvidia.optimus_prime.enable = cfg.prime.enable;
+    nvidia.prime.sync.enable = cfg.prime.enable;
     nvidia.modesetting.enable = cfg.prime.enable;
     opengl.driSupport32Bit = true;
 
     # Bus ID of the NVIDIA GPU. You can find it using lspci
-    nvidia.optimus_prime.nvidiaBusId = cfg.prime.nvidiaBusId;
+    nvidia.prime.nvidiaBusId = cfg.prime.nvidiaBusId;
 
     # Bus ID of the Intel GPU. You can find it using lspci
-    nvidia.optimus_prime.intelBusId = cfg.prime.intelBusId;
+    nvidia.prime.intelBusId = cfg.prime.intelBusId;
   };
 
   environment.systemPackages = lib.mkIf cfg.withCuda (with pkgs; [
